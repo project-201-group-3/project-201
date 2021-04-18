060ad67811953c4b827261c0a1dba93f8e1454bf
rujeenaal-zyoud <rujeenaalzoud@gmail.com>
2021-04-18 16:15:15 +0300

    addjs

--- js/add.js
+++ js/add.js
@@ -1,53 +1,117 @@
 'use strict';
 
 
-function formPage(Name,YearOfProduction,Rating,Type,file){
-this.Name=Name;
-this.YearOfProduction=YearOfProduction;
-this.Rating=Rating;
-this.Type=Type;
-this.file=file;
-addForm.push(this);
-}
-let image=document.getElementById('file');
-let addForm=[];
-let form=document.getElementById('form');
+function FormPage(Name, YearOfProduction, Rating, browser, file) {
+  this.Name = Name;
+  this.YearOfProduction = YearOfProduction;
+  this.Rating = Rating;
+  this.browser = browser;
+  this.file = file;
+  //create array as object 
+  FormPage.addForm.push(this);
 
-//create render function
-formPage.prototype.render=function(){
-image=image.file
+
 }
+FormPage.addForm = [];
+
+
+let image = document.getElementById('file');
+let form = document.getElementById('form');
+let submit = document.getElementById('submit');
+form.appendChild(submit);
+
+
+
+
+
 
 
 
 //create eventlistner
-let submit=document.getElementById('submit')
 form.addEventListener('submit', UserClick);
 
 
 
 //creat the UserClick function 
 function UserClick(event) {
-   
-    // if (event.target.id !== 'images') {
-   
-    //    prevent the default behaviour of refreshing the page
+  //    prevent the default behaviour of refreshing the page
 
   event.preventDefault();
 
-  let Name = event.target.Name.value;
-  console.log(Name);
-  let YearOfProduction = parseInt(event.target.YearOfProduction.value);
-  console.log(YearOfProduction);
-  let Rating = parseInt(event.target.Rating.value);
-  console.log(Rating);
-  let browser=event.target.browser.value;
-  console.log(browser);
-  
-  
+  let NameMSA = event.target.Name.value;
+  console.log(NameMSA);
+  let YearOfProductionMSA = parseInt(event.target.YearOfProduction.value);
+  console.log(YearOfProductionMSA);
+  let RatingMSA = parseInt(event.target.Rating.value);
+  console.log(RatingMSA);
+  let browserMSA = event.target.browser.value;
+  console.log(browserMSA);
 
-    //    if(event.target.id=='movie'){
-    //     Window.location='movies.html';
-    //    }
+  let fileMSA = event.target.file.value;
+  console.log(fileMSA);
 
-   }
+  const add = new FormPage(NameMSA, YearOfProductionMSA, RatingMSA, browserMSA, fileMSA);
+  console.log(add);
+  updateStorage();
+
+
+
+  if (browserMSA == 'movie') {
+    alert('Thank you for your updating ');
+    
+    window.location.href = "movies.html";
+    //let data = localStorage.getItem('product');
+
+
+  }
+  else if (browserMSA == 'series') {
+    alert('Thank you for your updating ');
+
+    window.location.href = "series.html";
+  }
+  else {
+    alert('Thank you for your updating ');
+
+    window.location.href = "anime.html";
+  }
+
+
+
+  submit.removeEventListener('submit', UserClick);
+
+}
+
+
+//create function for update storge
+function updateStorage() {
+
+  let arrayString = JSON.stringify(FormPage.addForm);
+  console.log(FormPage.addForm);
+  console.log(arrayString);
+  //set item by put key and value
+  localStorage.setItem('product', arrayString);
+}
+//updateStorage();
+
+// console.log(updateStorage());
+
+
+// get the data from local storge
+function getForm() {
+  // get the data from the local storage
+  let data = localStorage.getItem('product');
+  console.log(data);
+
+  //convert data back into a normal array of objects
+  let productData = JSON.parse(data);
+
+  console.log(productData);
+
+  // if the first time we visit the page, there will not be an array of objects inside the local storage so we should handle it here:
+  if (productData !== null) {
+    FormPage.addForm = productData;
+  }
+
+}
+
+getForm();
